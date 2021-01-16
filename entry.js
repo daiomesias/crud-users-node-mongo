33a2c89cf198d7a7940873238ad531cb66136630
Dario Erregue <derregue@cloudspace.com.ar>
2021-01-16 14:20:45 -0300

    env modified

--- entry.js
+++ entry.js
@@ -3,6 +3,8 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
 
@@ -15,12 +17,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/', (req1, res) => {
+app.post('/users', (req1, res) => {
+    let username = proccess.env.DB_USERNAME
+    let password = proccess.env.DB_PASSWORD
+    mongoose.connect('mongodb+srv://'+username+':'+password+'@cluster0.8iqby.gcp.mongodb.net/testdario?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true
+    }).then(() => {
+    console.log('connected to db')
+    }).catch((error) => {
+    console.log(error)
+                        })
     console.log(req1.body)
     res.send(req1.body.pais + 'Hello World Post!')
   })
 
-  app.get('/sucursales', (req1, res) => {
+
+//Reto crear un registro url
+// https://amljs.medium.com/crud-app-in-node-js-and-mongodb-1587923153d6
+
+
+
+  app.get('/users', (req1, res) => {
     //console.log(req1.body)
     let sucursales = [
   {
